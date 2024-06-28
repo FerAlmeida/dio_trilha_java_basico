71e8dd60a4ddfff6f3bffd3654edf2041d0d6472
Fernando Almeida <feralana@hotmail.com>
2024-06-28 13:05:30 -0300

    Projeto SmartTv Atulizado
    Foram adicionadas novas configurações para o projeto SmartTV

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,38 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void mudarCanal(){
+        canal ++;
+    }
+
+    public void diminuirCanal(){
+        canal --;
+    }
+
+    public void aumentarVolume(){
+        volume ++;
+        System.out.println("Aumentando o volume para: " + volume);
+        
+    }
+
+    public void diminuirVolume(){
+        volume --;
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
+
+    public void ligada(){
+        ligada=true;
+    }
+
+    public void desligada(){
+        ligada=false;
+    }
+
+
+}
