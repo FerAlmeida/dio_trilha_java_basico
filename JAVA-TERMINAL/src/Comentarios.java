085183846ca0d9270509da83e64e834a74d892ac
Fernando Almeida <feralana@hotmail.com>
2024-06-29 14:29:01 -0300

    Comentários em Classes e Métodos
    Foi criado um arquivo para entender o funcionamento de como se tratar os comentários em Classes e Métodos

---
+++ JAVA-TERMINAL/src/Comentarios.java
@@ -0,0 +1,28 @@
+public class Comentarios {
+
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+        * Eu sou um comentario
+         * que posso ser mais detalhadod
+         * quando necessário
+         */
+// Comentário de DOCUMENTAÇÃO, referindo-se a dois asteriscos (**), está se referindo a aspectos de documentação
+
+// Então temos a proposta de coloca-las dentro de um bloco de documentação, aqui declarado como METODO
+
+        /** 
+         * Estas duas estrelinhas acima
+         * é para identificar que você
+         * pretende elaborar um comentário
+         * a nível de documentação.
+         * Que incrível !!!
+         */
+    }
+
+    public void metodo(){
+
+    }
+    
+ }
